7358fd9efe0be6d4849e220ca6302b69c05efbee
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2023-11-26 15:56:12 -0300

    Inicialização do projeto

---
+++ src/main/java/com/example/mensageiro/MensageiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.mensageiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageiroApplication.class, args);
+	}
+
+}
